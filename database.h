b7dca6b65a0b9d4394518d73edd06fb82c427e9e
lafita17 <33731702+lafita17@users.noreply.github.com>
2020-12-29 20:29:57 +0100

    proxecto segmentado e primeiras funcions

---
+++ database.h
@@ -0,0 +1,18 @@
+
+ struct unPaciente{
+  char nombre[25];
+  char DNI[10];
+  int edad;
+  int fiebre;
+  int tos;
+  char sintoma;
+  int num;
+  struct unPaciente *sig;
+ };
+typedef struct unPaciente PACIENTE;
+typedef struct unPaciente * PPACIENTE;
+PPACIENTE p_register();
+int p_search();
+int p_discharge();
+int p_list();
+int p_mark();
