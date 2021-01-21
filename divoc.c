3237448c0a87e2f8e31cafeef10da957ee6a375d
lafita17 <33731702+lafita17@users.noreply.github.com>
2021-01-21 15:55:30 +0100

    cambios finais

--- divoc.c
+++ divoc.c
@@ -1,54 +1,92 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #include "inout.h"
 #include "database.h"
 
 int main () {
-  char c;
+  char c,texto[256];
   int x;
-  
-  stripe('-',60);
-  headline ("DIVOC_", '|',60);
-  stripe('-',60);
+  FILE *fichero;
 
   PPACIENTE inicio=NULL;
   PPACIENTE pAux=NULL;
   
- do { fprintf (stdout,"R) Register a patient\n");
+
+  if((fichero=fopen("patients.txt","r"))!=NULL)
+  {
+    while(fgets(texto,sizeof(texto),fichero)!=NULL)
+    {
+      PPACIENTE nuevo=(PPACIENTE)malloc(sizeof(PACIENTE));
+      sscanf(texto,"%s %s %d %d %d %c",nuevo->nombre,nuevo->DNI,&nuevo->edad,&nuevo->fiebre,&nuevo->tos,&nuevo->sintoma);
+      nuevo->sig=NULL;
+      if (inicio==NULL)
+      {
+      inicio=nuevo;
+      }
+      else 
+      {
+        pAux=inicio;
+        while(pAux->sig!=NULL) pAux=pAux->sig;
+        pAux->sig=nuevo;
+      }
+    }
+    fclose(fichero);
+  }	
+    
+  stripe('-',50);
+  headline ("DIVOC_", '|',50);
+  stripe('-',50);
+  printf("\n");
+
+ do { 
+   fprintf (stdout,"\nR) Register a patient\n");
   fprintf (stdout,"S) Search for a patient\n");
   fprintf (stdout,"D) Discharge a patient\n");
   fprintf (stdout,"L) List patients by age\n");
-  fprintf (stdout,"P) Mark positive\n");
-  fprintf (stdout,"X) Exit the program\n\n");
+  fprintf (stdout,"P) Mark positive\n\n");
+  fprintf (stdout,"X) Exit the program\n");
 
-  fprintf (stdout,"Choose an option:  (RSDLPX)\n");
-  fscanf (stdin," %s",&c);
+  c=get_character("Choose an option","RSDLPX");
 
   switch (toupper(c)){
 
   case 'R':
-    pAux=p_register(pAux);
-    if(pAux->num==1) inicio=pAux;
+    inicio=p_register(inicio);
+
     break;
   case 'S':
     p_search(inicio);
     break;
   case 'D':
     inicio=p_discharge(inicio);
-    pAux=inicio;
+    
     break;
   case 'L':
     p_list(inicio);
     break;
   case 'X':
-    x=yes_or_no("Seguro que quieres salir del programa?\n");
+    printf("\nExit\n");
+    x=yes_or_no("Are you sure you want to exit the program?(y/n):");
+    if (x==1){
+      fichero = fopen("patients.txt", "w");
+      if (fichero!=NULL)
+      {
+        pAux=inicio;
+        while(pAux!=NULL)
+        {
+          fprintf(fichero, "%s %s %d %d %d %c\n",pAux->nombre,pAux->DNI,pAux->edad,pAux->fiebre,pAux->tos,pAux->sintoma);
+          pAux=pAux->sig;
+        }
+      }
+      fclose(fichero);
+    }
     break;
   case 'P':
     p_mark(inicio);
     break;
   default:
-    fprintf(stdout,"Has seleccionado una opcion no valida\n");
     break;
   }
  }while (x!=1);
